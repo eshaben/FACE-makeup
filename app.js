b3e0756d1e5e6f03e372770104024b1dfa907fdb
Erin Shaben <eshaben@icloud.com>
2018-08-18 20:34:27 -0600

    remove brands not supported by API and update brand list to have ids

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-const brandsList = ['almay', 'annaSui', 'annabelle', 'benefit', "burt'sBees", 'butter london', 'cargo cosmetics', 'china glaze', 'covergirl', 'dalish', 'dior', 'dr. hauschka',
-'e.l.f.', 'essie', 'iman', "l'oreal", 'marcelle', 'maybelline', 'milani', 'mineral fusion', 'misa', 'mistura',
-'moov', 'nyx', 'orly', 'pacifica', 'physicians formula', 'piggy paint', 'pure anada', 'revlon', 'salon perfect', 'sante', 'sinful colours', 'smashbox', 'stila', 'suncoat', 'wet n wild', 'zorah']
-const faceItems = ['blush', 'bronzer', 'foundation']
-
+const brandsList = [
+{id: 1, brand: 'almay'},
+{id: 2, brand: 'annabelle'},
+{id: 3, brand: 'benefit'},
+{id: 4, brand: 'covergirl'},
+{id: 5, brand: 'dalish'},
+{id: 6, brand: 'dior'},
+{id: 7, brand: 'e.l.f.'},
+{id: 8, brand: 'essie'},
+{id: 9, brand: 'iman'},
+{id: 10, brand: "l'oreal"},
+{id: 11, brand: 'marcelle'},
+{id: 12, brand: 'maybelline'},
+{id: 13, brand: 'milani'},
+{id: 14, brand: 'misa'},
+{id: 15, brand: 'mistura'},
+{id: 16, brand: 'moov'},
+{id: 17, brand: 'nyx'},
+{id: 18, brand: 'orly'},
+{id: 19, brand: 'pacifica'},
+{id: 20, brand: 'revlon'},
+{id: 21, brand: 'sante'},
+{id: 22, brand: 'smashbox'},
+{id: 23, brand: 'stila'},
+{id: 24, brand: 'suncoat'},
+{id: 25, brand: 'zorah'}];
+const faceItems = ['blush', 'bronzer','foundation'];
+const makeupApiBaseUrl = 'http://makeup-api.herokuapp.com/api/v1/products.json';
 
 $('.products').append(`
   <h1 class="margin-top">Home Page... TBD</h1>
@@ -19,7 +42,7 @@ $('.brand').on('click', function(){
 
   for (var i = 0; i < brandsList.length; i++){
     $('.list-inline').append(`
-      <li class="col-3 brand-list list-inline-item"><button type="button" class="btn btn-link" id="${brandsList[i]}"> ${brandsList[i]} </button></li>
+      <li class="col-3 brand-list list-inline-item"><button type="button" class="btn btn-link" id="${brandsList[i].id}"> ${brandsList[i].brand} </button></li>
       `
     )
   }
@@ -60,11 +83,14 @@ $('.face').on('click', function(){
 
 })
 
-//ran into issue with spacing, apostrophes, and periods in brand names.. Need to
-//use regex to get rid of above things -- think about best way to approach and come back to it
-
 for (var i = 0; i < brandsList.length; i++){
-  $(document).on('click', '#' + `${brandsList[i]}`, function(){
-    console.log(this.id);
+  $(document).on('click', '#' + brandsList[i].id, function(){
+    getDataByBrand(brandsList[this.id - 1].brand);
+  })
+}
+
+function getDataByBrand(brand){
+  $.get(`${makeupApiBaseUrl}?brand=${brand}`, function(data){
+    console.log(data);
   })
 }
